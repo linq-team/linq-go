d55b4ec63b7d9e28c38573b1b17589334d893c64
Josh Flayhart <jflayhart@users.noreply.github.com>
2026-09-19 17:01:53 -0500

    fix: update webhook header name examples to reflect current api (#164)
    Co-authored-by: linq-sdks-bot <linq-sdks-bot@linqapp.com>

--- webhooksubscription_test.go
+++ webhooksubscription_test.go
@@ -30,8 +30,8 @@ func TestWebhookSubscriptionNewWithOptionalParams(t *testing.T) {
 		SubscribedEvents: []linqgo.WebhookEventType{linqgo.WebhookEventTypeMessageSent, linqgo.WebhookEventTypeMessageDelivered, linqgo.WebhookEventTypeMessageRead},
 		TargetURL:        "https://webhooks.example.com/linq/events",
 		PhoneNumbers:     []string{"+12025551234", "+12025559876"},
-		RoutingIDHeader:  linqgo.String("x-openai-chat-id"),
-		RoutingKeyHeader: linqgo.String("x-openai-thread-route"),
+		RoutingIDHeader:  linqgo.String("X-Chat-Id"),
+		RoutingKeyHeader: linqgo.String("X-Route-Token"),
 	})
 	if err != nil {
 		var apierr *linqgo.Error
@@ -84,8 +84,8 @@ func TestWebhookSubscriptionUpdateWithOptionalParams(t *testing.T) {
 		linqgo.WebhookSubscriptionUpdateParams{
 			IsActive:         linqgo.Bool(true),
 			PhoneNumbers:     []string{"+12025551234"},
-			RoutingIDHeader:  linqgo.String("x-openai-chat-id"),
-			RoutingKeyHeader: linqgo.String("x-openai-thread-route"),
+			RoutingIDHeader:  linqgo.String("X-Chat-Id"),
+			RoutingKeyHeader: linqgo.String("X-Route-Token"),
 			SubscribedEvents: []linqgo.WebhookEventType{linqgo.WebhookEventTypeMessageSent, linqgo.WebhookEventTypeMessageDelivered},
 			TargetURL:        linqgo.String("https://webhooks.example.com/linq/events"),
 		},
